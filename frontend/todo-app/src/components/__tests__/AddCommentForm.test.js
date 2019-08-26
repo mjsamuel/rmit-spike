1152a3bbcdc44b4e07a40878f4b76d76af0a6111
Samuel Dowling <samuel.dowling@protonmail.com>
2019-08-26 11:15:16 +1000

    Added additional unit tests as per testing matrix

--- frontend/todo-app/src/components/__tests__/AddCommentForm.test.js
+++ frontend/todo-app/src/components/__tests__/AddCommentForm.test.js
@@ -18,20 +18,33 @@ describe('AddCommentForm', () => {
 		component = mount(<AddCommentForm {...form} />)
 	})
 
-	it('should exist', () => {
+	it('AddCommentForm should exist', () => {
 		expect(component).toBeTruthy();
 		expect(component.props().thread_id).toEqual(form.thread_id)
 		expect(component.props().isReply).toEqual(form.isReply)
 		expect(component.props().isReport).toEqual(form.isReport)
 	})
 
-	// it('display primary channel when props are passed', () => {
-	// 	let channel = component.find('.channel');
-	// 	expect(channel.text()).toEqual("c/" + thread.primary_channel);
-	// })
+	it('AddCommentForm state initialised to null', () => {
+		// console.log(component.state())
+		expect(component.state().value).toEqual('')
+	})
 
-	// it('display thread title when props are passed', () => {
-	// 	let title = component.find('.thread-title');
-	// 	expect(title.text()).toEqual(thread.title);
-	// })
+	it('AddCommentForm report placeholder', () => {
+		const report_phrase = "What would you like to report? Please include as much detail as possible"
+		expect(component.state().placeholder).toBeDefined()
+		expect(component.state().placeholder).toEqual(report_phrase)
+	})
+
+	it('AddCommentForm report placeholder', () => {
+		const reply_form = {
+			thread_id: 1,
+			isReply: true,
+			isReport: false,
+		}
+		const component = mount(<AddCommentForm {...reply_form} />)
+		const reply_phrase = "What are your thoughts?"
+		expect(component.state().placeholder).toBeDefined()
+		expect(component.state().placeholder).toEqual(reply_phrase)
+	})
 })
--- frontend/todo-app/src/components/__tests__/CommentComponent.test.js
+++ frontend/todo-app/src/components/__tests__/CommentComponent.test.js
@@ -31,13 +31,74 @@ describe('CommentComponent', () => {
 
 	})
 
-	// it('display primary channel when props are passed', () => {
-	// 	let channel = component.find('.channel');
-	// 	expect(channel.text()).toEqual("c/" + thread.primary_channel);
-	// })
+	it('throws error when not given props', () => {
+		// Not implemented
+		component = mount(<CommentComponent />)
+		let error = component.find('.error')
+		expect(error.text()).toEqual("Something went wrong.");
+	})
 
-	// it('display thread title when props are passed', () => {
-	// 	let title = component.find('.thread-title');
-	// 	expect(title.text()).toEqual(thread.title);
-	// })
+	it('renders author name', () => {
+		let author = component.find('.author')
+		expect(author.text()).toEqual("u/" + comment.author);
+	})
+
+	it('renders number of spikes', () => {
+		let spikes = component.find('.spikes')
+		expect(spikes.text()).toEqual(comment.spikes + " Spikes");
+	})
+
+	it('renders timeDelta correctly', () => {
+		let timeDelta = component.find('.timeDelta')
+		expect(timeDelta.text()).toEqual(comment.timeNumber + " "  + comment.timeUnit + " ago");
+	})
+
+	it('renders comment content correctly', () => {
+		let content = component.find('.content')
+		expect(content.text()).toEqual(comment.content);
+	})
+
+	it('upspike activation changes state.upspiked to true, state.downspiked to false', () => {
+		/**
+		 * TODO: Implement this
+		 * Currently not sure how to test a function within a component. When time permits, research:
+		 * https://stackoverflow.com/questions/25533036/test-a-react-component-function-with-jest
+		 */
+
+		 //intentional fail:
+		 expect(component).toBeUndefined()
+	})
+
+	it('downspike activation changes state.downspiked to true, state.upspiked to false', () => {
+		/**
+		 * TODO: Implement this
+		 * Currently not sure how to test a function within a component. When time permits, research:
+		 * https://stackoverflow.com/questions/25533036/test-a-react-component-function-with-jest
+		 */
+
+ 		 //intentional fail:
+		 expect(component).toBeUndefined()
+	})
+
+	it('reply activation sets state.replyActive to true, state.reportActive to false', () => {
+		/**
+		 * TODO: Implement this
+		 * Currently not sure how to test a function within a component. When time permits, research:
+		 * https://stackoverflow.com/questions/25533036/test-a-react-component-function-with-jest
+		 */
+
+ 		 //intentional fail:
+		 expect(component).toBeUndefined()
+	})
+
+	it('report activation sets state.reportActive to true, state to replyActive to false', () => {
+		/**
+		 * TODO: Implement this
+		 * Currently not sure how to test a function within a component. When time permits, research:
+		 * https://stackoverflow.com/questions/25533036/test-a-react-component-function-with-jest
+		 */
+
+ 		 //intentional fail:
+		 expect(component).toBeUndefined()
+	})
 })
