875bca2f482bdbbb44b7c5500fc072bfff40bd9a
Samuel Dowling <samuel.dowling@protonmail.com>
2019-09-04 16:14:55 +1000

    Turned off CI env for node build, added authentication for docker agent

--- deploy-backend-staging.sh
+++ deploy-backend-staging.sh
@@ -12,6 +12,7 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME_STG
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_STG
 
+cat ${HOME}/gcloud-service-key.json | docker login -u _json_key --password-stdin https://gcr.io
 docker push gcr.io/${PROJECT_NAME_STG}/${BACKEND_DOCKER_IMAGE_NAME}
 
 yes | gcloud container images add-tag gcr.io/${PROJECT_NAME_STG}/${BACKEND_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_STG}/${BACKEND_DOCKER_IMAGE_NAME}:latest
