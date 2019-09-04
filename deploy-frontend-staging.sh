82e0e6b393205d88716845cb885578a338263503
Samuel Dowling <samuel.dowling@protonmail.com>
2019-09-04 15:04:19 +1000

    Removed gcloud from docker push and attempt to segregate deploy into frontend and backend consitutuents

--- deploy-frontend-staging.sh
+++ deploy-frontend-staging.sh
@@ -4,7 +4,7 @@ set -e
 
 docker build -t gcr.io/${PROJECT_NAME_STG}/${FRONTEND_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT $FRONTEND_BUILD_DIR
 
-echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
+echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i - > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 gcloud --quiet config set project $PROJECT_NAME_STG
@@ -12,7 +12,7 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME_STG
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_STG
 
-gcloud docker push gcr.io/${PROJECT_NAME_STG}/${FRONTEND_DOCKER_IMAGE_NAME}
+docker push gcr.io/${PROJECT_NAME_STG}/${FRONTEND_DOCKER_IMAGE_NAME}
 
 yes | gcloud container images add-tag gcr.io/${PROJECT_NAME_STG}/${FRONTEND_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_STG}/${FRONTEND_DOCKER_IMAGE_NAME}:latest
 
--- deploy-staging.sh
+++ deploy-staging.sh
@@ -13,8 +13,8 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME_STG
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_STG
 
-gcloud docker push gcr.io/${PROJECT_NAME_STG}/${FRONTEND_DOCKER_IMAGE_NAME}
-gcloud docker push gcr.io/${PROJECT_NAME_STG}/${BACKEND_DOCKER_IMAGE_NAME}
+docker push gcr.io/${PROJECT_NAME_STG}/${FRONTEND_DOCKER_IMAGE_NAME}
+docker push gcr.io/${PROJECT_NAME_STG}/${BACKEND_DOCKER_IMAGE_NAME}
 
 yes | gcloud container images add-tag gcr.io/${PROJECT_NAME_STG}/${FRONTEND_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_STG}/${FRONTEND_DOCKER_IMAGE_NAME}:latest
 yes | gcloud container images add-tag gcr.io/${PROJECT_NAME_STG}/${BACKEND_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_STG}/${BACKEND_DOCKER_IMAGE_NAME}:latest
