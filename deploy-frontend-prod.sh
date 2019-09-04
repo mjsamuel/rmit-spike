b3f81646f6e083748070066dd304b15a92ee9d84
Samuel Dowling <samuel.dowling@protonmail.com>
2019-09-04 18:02:35 +1000

    reset staging branch to develop, populated production deployment scripts

--- deploy-frontend-prod.sh
+++ deploy-frontend-prod.sh
@@ -3,7 +3,6 @@
 set -e
 
 docker build -t gcr.io/${PROJECT_NAME_PRD}/${FRONTEND_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT $FRONTEND_BUILD_DIR
-docker build -t gcr.io/${PROJECT_NAME_PRD}/${BACKEND_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT $BACKEND_BUILD_DIR
 
 echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode -i - > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
@@ -13,14 +12,15 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
 
-gcloud docker push gcr.io/${PROJECT_NAME_PRD}/${FRONTEND_DOCKER_IMAGE_NAME}
-gcloud docker push gcr.io/${PROJECT_NAME_PRD}/${BACKEND_DOCKER_IMAGE_NAME}
+cat ${HOME}/gcloud-service-key.json | docker login -u _json_key --password-stdin https://gcr.io
+docker push gcr.io/${PROJECT_NAME_PRD}/${FRONTEND_DOCKER_IMAGE_NAME}
 
 yes | gcloud container images add-tag gcr.io/${PROJECT_NAME_PRD}/${FRONTEND_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_PRD}/${FRONTEND_DOCKER_IMAGE_NAME}:latest
-yes | gcloud container images add-tag gcr.io/${PROJECT_NAME_PRD}/${BACKEND_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_PRD}/${BACKEND_DOCKER_IMAGE_NAME}:latest
 
 kubectl config view
 kubectl config current-context
 
 kubectl set image deployment/${FRONTEND_KUBE_DEPLOYMENT_NAME} ${FRONTEND_KUBE_DEPLOYMENT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${FRONTEND_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
-kubectl set image deployment/${BACKEND_KUBE_DEPLOYMENT_NAME} ${BACKEND_KUBE_DEPLOYMENT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${BACKEND_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
+
+# sleep 30
+# npm run e2e_test
