e7573e9e056bbcab45360448b78ea20e9649ae37
s3717393 <s3717393@student.rmit.edu.au>
2019-09-04 17:28:35 +1000

    Added subsribe and add thread button

--- frontend/todo-app/src/api/todo/ChannelDataService.js
+++ frontend/todo-app/src/api/todo/ChannelDataService.js
@@ -2,6 +2,32 @@ import axios from 'axios'
 import { API_URL, JPA_API_URL, DATA_API_URL } from '../../Constants'
 
 class ChannelDataService {
+  retrieveChannelThreads(channel_id) {
+      console.log('retrieveChannelThreads API endpoint called')
+      // return axios.get(`${DATA_API_URL}/thread?chan=${channel_id}`)
+      const response = `{
+        "channelName": "c/sept",
+        "threads": [
+          {
+            "id": "001",
+            "title": "Thread Title",
+            "author": "u/author",
+            "noComments": 0,
+            "upspikes": 0
+          },
+          {
+            "id": "002",
+            "title": "The Rise and Fall of SEPT at RMIT",
+            "author": "u/john-smith",
+            "noComments": 13,
+            "upspikes": 30
+          }
+        ]
+      }`
+      
+      return JSON.parse(response)
+  }
+
   createChannel(username, channelName, visibility) {
     console.log('createChannel API endpoint called')
     // axios.post(`${API_URL}/channel`, {
@@ -18,5 +44,7 @@ class ChannelDataService {
     return JSON.parse(response)
   }
 }
+
+
 
 export default new ChannelDataService()
--- frontend/todo-app/src/components/todo/ChannelComponent.jsx
+++ frontend/todo-app/src/components/todo/ChannelComponent.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import ThreadDataService from '../../api/todo/ThreadDataService.js'
+import ChannelDataService from '../../api/todo/ChannelDataService.js'
 import './ChannelComponent.css';
 import { FaRegComment, FaAngleUp } from 'react-icons/fa';
 
@@ -22,8 +22,12 @@ class ChannelComponent extends Component {
     this.state = {
       channelName: '',
       threads: [],
+      subscribed: false,
       hasErrorOccured: false
     }
+
+    this.subscribeClicked = this.subscribeClicked.bind(this)
+    this.newThreadClicked = this.newThreadClicked.bind(this)
   }
 
   /**
@@ -32,9 +36,7 @@ class ChannelComponent extends Component {
    */
   componentDidMount() {
     const { match: { params } } = this.props;
-    console.log(params.channelId)
-
-    var data = ThreadDataService.retrieveChannelThreads(params.channelId)
+    var data = ChannelDataService.retrieveChannelThreads(params.channelId)
     this.setState({
       channelName: data.channelName,
       threads: data.threads
@@ -42,30 +44,54 @@ class ChannelComponent extends Component {
   }
 
   /**
+   * Notifies the backend that the user has subscribed/unsubscribed and changes the style
+   * of the button accordigly by flipping the boolean that indicates the users subscription
+   * state
+   */
+  subscribeClicked() {
+    this.setState({subscribed: !this.state.subscribed})
+  }
+
+  /**
+   * Redirects to the new thread page, passing a channel id as a paramater
+   */
+  newThreadClicked() {
+    const { match: { params } } = this.props;
+    this.props.history.push(`/new-thread`, {channelId: params.channelId})
+  }
+  /**
    * Renders the thread HTML
    */
   render() {
     return (
-      <div className="thread-list">
-        <h1>{this.state.channelName}</h1>
-        {this.state.threads.map((thread, index) => {
-          return (
-            <div className="thread-card">
-              <a href={"/thread/" + thread.id} className="title">{thread.title}</a><br/>
-              <a href={"/user/" + thread.author} className="author">{thread.author}</a><br/>
-              <div className="details-bar">
-                <span><FaRegComment/> {thread.noComments} comments | <FaAngleUp/> {thread.upspikes} Upspikes</span>
+      <>
+        <div className="thread-list">
+          <h1>{this.state.channelName}</h1>
+          {this.state.threads.map((thread, index) => {
+            return (
+              <div className="thread-card" id={thread.id}>
+                <a href={"/thread/" + thread.id} className="title">{thread.title}</a><br/>
+                <a href={"/user/" + thread.author} className="author">{thread.author}</a><br/>
+                <div className="details-bar">
+                  <span><FaRegComment/> {thread.noComments} comments | <FaAngleUp/> {thread.upspikes} Upspikes</span>
+                </div>
               </div>
-            </div>
-            )
-          })
-        }
-        <ul className="pagination">
-          <li className="page-item"><a className="page-link" href="#">Previous</a></li>
-          <li className="page-item"><a className="page-link" href="?page1">1</a></li>
-          <li className="page-item"><a className="page-link" href="#">Next</a></li>
-        </ul>
-      </div>
+              )
+            })
+          }
+          <ul className="pagination">
+            <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+            <li className="page-item"><a className="page-link" href="?page1">1</a></li>
+            <li className="page-item"><a className="page-link" href="#">Next</a></li>
+          </ul>
+        </div>
+        <div className="side-panel">
+          <button id="subsribe-btn" className={this.state.subscribed ? "btn btn-secondary ml-2" : "btn btn-success ml-2"} onClick={this.subscribeClicked}>
+            {this.state.subscribed ? "Unsubscribe" : "Subscribe"}
+          </button>
+          <button className="btn btn-success ml-2" onClick={this.newThreadClicked}>New Thread</button><br/>
+        </div>
+      </>
     )
   }
 }
