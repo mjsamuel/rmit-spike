9a48320ca9e860c045a50414392a9ea9590bb6d0
Ali <s3661626@student.rmit.edu.au>
2019-09-27 00:37:43 +1000

    User Class

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/model/User.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/model/User.java
@@ -1,33 +1,94 @@
 package com.sept.rest.webservices.restfulwebservices.model;
 
+import javax.management.InvalidAttributeValueException;
+
 public class User {
 
-	private String username;
+	private String email;
 	private String password;
+	private String studentNumber;
 	private String firstName;
 	private String lastName;
+	private boolean archived;
+	private boolean admin;
+	private int upspikes;
+	private int downspikes;
+
 		
-	public User(String username, String password, String firstname, String lastname) {
-		this.username = username;
+	public User(String email, String password, String studentNumber, String firstName, String lastName, boolean archived, boolean admin, int upspikes, int downspikes) {
+		this.email = email;
 		this.password = password;
-		this.firstName = firstname;
-		this.lastName = lastname;
+		this.studentNumber = studentNumber;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.admin = false;
+		this.archived = false;
+		this.upspikes = 0;
+		this.downspikes = 0;
+
 		
 	}
 
-	public String getUsername() {
-		return this.username;
-	}
+	public String getEmail() { return this.email;}
+
+	public void setEmail(String email) {this.email = email;}
 
 	public String getPassword() {
 		return this.password;
 	}
 
-	public String getFristname() {
-		return this.firstName;
+	public void setPassword(String password) {this.password = password;}
+
+	public String getStudentNumber() {
+		return this.studentNumber;
 	}
 
-	public String getLastName() {
-		return this.lastName;
+	public void setStudentNumber(String studentNumber) {this.studentNumber = studentNumber;}
+
+	public String getFirstName() { return this.firstName; }
+
+	public void setFirstName() {this.firstName = firstName;}
+
+	public String getLastName() { return this.lastName; }
+
+	public void setLastNameName() {this.lastName = lastName;}
+
+	public boolean isAdmin() {
+		return admin;
 	}
-}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+
+	public boolean isArchived() {
+		return archived;
+	}
+
+	public void setArchived(boolean archived) {
+		this.archived = archived;
+	}
+
+	public int getUpspikes() {
+		return upspikes;
+	}
+
+	public void setUpspikes(int upspikes) throws InvalidAttributeValueException {
+		if(upspikes >= 0)
+			this.upspikes=upspikes;
+		else
+			throw new InvalidAttributeValueException("Number of upspikes must be positive.");
+	}
+
+	public int getDownspikes() {
+		return downspikes;
+	}
+
+	public void setDownspikes(int downspikes) throws InvalidAttributeValueException {
+		if(downspikes >= 0)
+			this.downspikes = downspikes;
+		else
+			throw new InvalidAttributeValueException("Number of downspikes must be positive.");
+	}
+
+
