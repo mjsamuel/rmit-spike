2a1d5af06a559d21623a01bcb9e391d61cc16b2a
s3717393 <s3717393@student.rmit.edu.au>
2019-09-29 17:00:26 +1000

    Fixed failing tests in backend

--- restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/channel/ChannelTests.java
+++ restful-web-services/src/test/java/com/sept/rest/webservices/restfulwebservices/channel/ChannelTests.java
@@ -45,9 +45,9 @@ public class ChannelTests {
 
 		List<Thread> threads = new ArrayList<>();
 		Thread t1 = new Thread((long) 1, "How to subscribe to channel?",
-				"I found a channel I like and want to subscribe to it.", false, 245, 5, "ads");
+				"I found a channel I like and want to subscribe to it.", false, 245, 5, "ads", null, null);
 		Thread t2 = new Thread((long) 2, "How to delete previous thread",
-				"I found a solution to my previous thread and want to delete it.", true, 123, 23, "ads");
+				"I found a solution to my previous thread and want to delete it.", true, 123, 23, "ads", null, null);
 
 		threadRepository.save(t1);
 		threadRepository.save(t2);
@@ -78,7 +78,7 @@ public class ChannelTests {
  	public void testAddThreadToChannel() {
 		Channel c1 = new Channel((long) 1, "SEPT", Visibility.SHARED, true);
 		Thread t1 = new Thread((long) 1, "How to subscribe to channel?",
-				"I found a channel I like and want to subscribe to it.", false, 245, 5, "ads");
+				"I found a channel I like and want to subscribe to it.", false, 245, 5, "ads", null, null);
 
  		assertNotNull(c1);
  		assertNotNull(t1);
@@ -114,7 +114,7 @@ public class ChannelTests {
  	public void testRemoveThreadFromChannel() {
 		Channel c1 = new Channel((long) 1, "SEPT", Visibility.SHARED, true);
 		Thread t1 = new Thread((long) 1, "How to subscribe to channel?",
-				"I found a channel I like and want to subscribe to it.", false, 245, 5, "ads");
+				"I found a channel I like and want to subscribe to it.", false, 245, 5, "ads", null, null);
 
  		assertNotNull(c1);
  		assertNotNull(t1);
