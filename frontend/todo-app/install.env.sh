e752bcaddbdac49da8bd0e34843c7a0db74304cc
Samuel Dowling <samuel.dowling@protonmail.com>
2019-09-30 10:37:24 +1000

    Added 'JPA_API_URL' to fix compilation errors

--- frontend/todo-app/install.env.sh
+++ frontend/todo-app/install.env.sh
@@ -1,4 +1,5 @@
 cat > src/Constants.js <<EOL
-export const API_URL = 'http://localhost:8080'
+export const API_URL = 'http://${API_HOST}'
+export const JPA_API_URL = 'http://${API_HOST}/jpa'
 export const DATA_API_URL = 'http://${API_HOST}/api'
 EOL
