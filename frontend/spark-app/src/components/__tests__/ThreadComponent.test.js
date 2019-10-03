f04a9917ee6a64217a98e0570c21597e5299145b
Samuel Dowling <samuel.dowling@protonmail.com>
2019-10-03 23:40:28 +1000

    Mocked axios api to get thread frontend tests functional, fixed some typos in Thread constructor on backend

--- frontend/spark-app/src/components/__tests__/ThreadComponent.test.js
+++ frontend/spark-app/src/components/__tests__/ThreadComponent.test.js
@@ -1,72 +1,122 @@
-import React from 'react'
+import React from 'react';
 import ReactTestUtils from 'react-dom/test-utils'; 
-import { shallow, mount } from 'enzyme'
-import ThreadComponent from '../ThreadComponent'
-
+import { shallow, mount } from 'enzyme';
+import ThreadComponent from '../ThreadComponent';
+import MockAdapter from 'axios-mock-adapter';
+import axios from '../../axios.js'
+import { DATA_API_URL } from '../../Constants'
 
 describe('ThreadComponent', () => {
 	var component, thread;
 
-	// beforeEach(() => {
-	// 	thread = {
-	// 		"id": 1,
-	// 		"author": "John Smith",
-	// 		"title": "The Rise and Fall of SEPT at RMIT",
-	// 		"primary_channel": "sept",
-	// 	}
-	// })
-	// beforeEach(() => {
-	// 	// component = mount(<ThreadComponent {...thread} />)
-	// 	component = mount(<ThreadComponent />)
-	// })
+	beforeAll(() => {
+		var mock = new MockAdapter(axios.instance)
+		const thread_id = 1;
+		const channel_id = 1;
+		const user_id = 1;
 
-	it('should exist', () => {
-		expect(true).toBeTruthy();
-		// expect(component).toBeTruthy();
-		// expect(component.props().id).toEqual(thread.id)
-		// expect(component.props().author).toEqual(thread.author)
-		// expect(component.props().title).toEqual(thread.title)
-		// expect(component.props().primary_channel).toEqual(thread.primary_channel)
+		const threadServiceData = {
+			title: "The Rise and Fall of SEPT at RMIT",
+			content: "What is even going on",
+			timeDelta: "2 minutes ago",
+			// primary_channel: "sept",
+			// tagged_channels: [1, 2, 3],
+			channelId: channel_id,
+			authorId: user_id
+		}
+		const userServiceData = {
+			username: "smithj"
+		}
+		const channelServiceData = {
+			channelName: "sept"
+		}
+		const commentServiceData = [
+			{
+				id: 1,
+				datetime: "2019-09-16T13:42:32.000+0000",
+				authorId: 0,
+				upspikes: 3,
+				downspikes: 1,
+				content: "This is a more recent post!",
+				replyId: 0,
+				threadId: 1,
+				archived: false,
+				spikes: 2,
+				spikeRatio: 0.75,
+				timeDelta: "2 weeks ago"
+			},
+			{
+				id: 2,
+				datetime: "2019-09-16T18:42:32.000+0000",
+				authorId: 0,
+				upspikes: 30,
+				downspikes: 14,
+				content: "This is an even more recent post!",
+				replyId: 0,
+				threadId: 1,
+				archived: false,
+				spikes: 16,
+				spikeRatio: 0.6818182,
+				timeDelta: "2 weeks ago"
+			},
+			{
+				id: 3,
+				datetime: "2019-09-30T00:57:56.000+0000",
+				authorId: 1,
+				upspikes: 0,
+				downspikes: 0,
+				content: "This is a test comment",
+				replyId: null,
+				threadId: 1,
+				archived: false,
+				spikes: 0,
+				spikeRatio: 1.0,
+				timeDelta: "3 days ago"
+			}
+		]
+
+		const match = { params: { id: 1 }}
+
+		mock.onGet(`${DATA_API_URL}/channel/${channel_id}?user_id=${user_id}`).reply(200, channelServiceData)
+		mock.onGet(`${DATA_API_URL}/thread/${thread_id}`).reply(200, threadServiceData)
+		mock.onGet(`${DATA_API_URL}/user/${user_id}`).reply(200, userServiceData)
+		mock.onGet(`${DATA_API_URL}/thread/${thread_id}/comment`).reply(200, commentServiceData)
+		component = mount(<ThreadComponent match={match}/>)
 
 	})
 
-	// it('display primary channel when props are passed', () => {
-	// 	let channel = component.find('.channel');
-	// 	expect(channel.text()).toEqual("c/" + thread.primary_channel);
-	// })
+	it('should exist', () => {
+		expect(component).toBeTruthy();
+		expect(component.props().match.params.id).toEqual(1)
+	})
 
-	// it('display thread title when props are passed', () => {
-	// 	let title = component.find('.thread-title');
-	// 	expect(title.text()).toEqual(thread.title);
-	// })
+	it('display primary channel when props are passed', () => {
+		let channel = component.find('.channel');
+		expect(channel.text()).toEqual("c/" + "sept");
+	})
 
-	// it('Author text is rendered from API call', () => {
-	// 	const timeDelta = "1 week"
+	it('display thread title when props are passed', () => {
+		let title = component.find('.thread-title');
+		expect(title.text()).toEqual("The Rise and Fall of SEPT at RMIT");
+	})
 
-	// 	let author = component.find('.thread-author');
-	// 	expect(author.text()).toEqual("Posted by u/"+thread.author + " " + timeDelta + " ago");
-	// })
+	it('Author text is rendered from API call', () => {
+		let author = component.find('.thread-author');
+		expect(author.text()).toEqual("Posted by u/smithj 2 minutes ago");
+	})
 
 
-	// it('Thread contents renders', () => {
-	// 	const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id semper risus in hendrerit. Facilisis magna etiam tempor orci eu lobortis elementum nibh. Integer quis auctor elit sed vulputate. Arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales. Pulvinar sapien et ligula ullamcorper malesuada. Mi in nulla posuere sollicitudin aliquam. Auctor augue mauris augue neque gravida in fermentum et sollicitudin. Tincidunt nunc pulvinar sapien et. Libero id faucibus nisl tincidunt eget nullam non. Dictum sit amet justo donec enim diam vulputate. Sodales ut etiam sit amet nisl purus in mollis nunc. Vulputate mi sit amet mauris commodo. Diam vulputate ut pharetra sit amet aliquam id. Etiam non quam lacus suspendisse faucibus interdum posuere. Dolor morbi non arcu risus. Vulputate enim nulla aliquet porttitor lacus. Malesuada fames ac turpis egestas sed tempus. Tincidunt lobortis feugiat vivamus at. Enim neque volutpat ac tincidunt vitae semper quis. Vestibulum lorem sed risus ultricies tristique. Quis imperdiet massa tincidunt nunc pulvinar sapien et ligula. Senectus et netus et malesuada fames ac turpis egestas. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Mauris nunc congue nisi vitae suscipit tellus mauris a. Duis ut diam quam nulla porttitor. Viverra aliquet eget sit amet tellus cras. Tincidunt augue interdum velit euismod in pellentesque massa. Quam vulputate dignissim suspendisse in est.";
-
-	// 	let threadContent = component.find('.thread-contents');
-	// 	expect(threadContent.text()).toEqual(content);
-	// })	
-	// /**
-	//  * Functionality not implemented yet; API not functional
-	//  */
-	// it('display error when props not passed', () => {
-	// 	component = mount(<ThreadComponent />)
-	// 	let node = component.find('.error');
-	// 	// expect(node.text()).toEqual('Sorry. Thread doesn\'t exist.');
-	// })
-
-	// it('CommentsComponents render from API call', () => {
-	// 	let node = component.find('CommentComponent');
-	// 	console.log(node)
-	// 	expect(node.length).toEqual(4);
-	// })
+	it('Thread contents renders', () => {
+		let threadContent = component.find('.thread-contents');
+		expect(threadContent.text()).toEqual("What is even going on");
+	})	
+	/**
+	 * Functionality not implemented yet; Error checking
+	 */
+	it('display error when props not passed', () => {
+		// component = mount(<ThreadComponent />)
+		// let node = component.find('.error');
+		// expect(node.text()).toEqual('Sorry. Thread doesn\'t exist.');
+	})
 
 })
